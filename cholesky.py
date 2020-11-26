051255935b0a828f7665e06179c6f4eefbadd7de
NicoPelayo <nifernandez@itba.edu.ar>
2020-11-26 15:55:23 -0300

    Cholesky

--- cholesky.py
+++ cholesky.py
@@ -7,8 +7,8 @@ import numpy as np
 # Toma la matriz A de nxn simétrica definida positiva y devuelve una matriz G de nxn triangular inferior tal que G.G^T=A
 # ----------------------------------------------------------------------------------------------------------------------
 def cholesky(A):
-    (n, m) = A.shape
-    avas = np.linalg.eigvals(A)  # Si algun autovalor es <=0, da error
+    (n, m) = A.shape                #Guardo dimensiones de A
+    avas = np.linalg.eigvals(A)     # Si algun autovalor es <=0, da error
     for i in avas:
         if i < np.finfo(float).eps:
             print("La matriz no es definida positiva")
@@ -16,21 +16,20 @@ def cholesky(A):
         if not (A == A.T).all():
             print("La matriz no es simétrica")
             return "ERROR"
-    G = np.zeros((n, n))
+    G = np.zeros((n, n))            # defino matriz G a calcular
     for i in range(n):
         for j in range(n):
+            sum = 0
             if i == j:
-                sumsq = 0
                 if i != 0:
-                    for l in range(i):
-                        sumsq += (G[i, l]) ** 2
-                G[i, i] = np.sqrt((A[i, i] - sumsq))
+                    for l in range(i):          # calculo de elementos en la diagonal de G
+                        sum += (G[i, l]) ** 2
+                G[i, i] = np.sqrt((A[i, i] - sum))
             elif i > j:
-                sumG = 0
                 if j != 0:
                     for l in range(j):
-                        sumG += G[i, l] * G[j, l]
-                G[i, j] = (A[i, j] - sumG) / G[j, j]
+                        sum += G[i, l] * G[j, l]
+                G[i, j] = (A[i, j] - sum) / G[j, j]     # calculo de elementos que no estan en la diagonal de G
     return G
 ########################################################################################################################
 
@@ -44,8 +43,8 @@ def solve_trig(A, b):
         print("La matriz ingresada no es cuadrada o no son compatibles las dimensiones de A y b")
         return "ERROR"
 
-    if b.shape[0] < 2:
-        b = np.atleast_2d(b).T  # para convertir vectores 1-D de n a 2-d de nx1
+
+    b = np.atleast_2d(b).T          # convierto b a forma de vector vertical
 
     n = b.shape[0]
     x = np.zeros((n, 1))
@@ -77,10 +76,18 @@ def leastsq(A, b):
     if A.shape[0] != b.shape[0]:
         print("Sistema incompatible")
         return "ERROR"
-    B = np.dot(np.transpose(A), A)
-    G = cholesky(B)
-    C = np.dot(np.transpose(A), b)
-    y = solve_trig(G, C)
-    X = solve_trig(np.transpose(G), y)
-    return X
+    B = np.dot(np.transpose(A), A)                  # B= At . A
+    G = cholesky(B)                                 # B = G . Gt
+    y = solve_trig(G, np.dot(np.transpose(A), b))   # G.y = At.b
+    x = solve_trig(np.transpose(G), y)              # Gt.x = y
+    return x
 ########################################################################################################################
+A=np.array( [[1,3],
+            [2,4],
+             [8,9]])
+
+
+b=np.array([1,2,10])
+
+print(np.linalg.lstsq(A,b))
+print(leastsq(A,b))
