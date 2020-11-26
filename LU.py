4e7b552efa01455e345019831fa509b0e16d3e23
AMartin99 <70243820+AMartin99@users.noreply.github.com>
2020-11-26 15:52:10 -0300

    LU de nicoT que funciona tienen q importar copy

--- LU.py
+++ LU.py
@@ -1,11 +1,16 @@
-def LU(mat):
-    L = np.identity(mat[0].size)
-    U = mat.astype(float)
-
-    for i in range(mat[0].size - 1):
-        m = U[:, i] / U[i][i]
-        for j in range(i + 1, mat[0].size):
-            L[j][i] = m[j]
-
-            U[j] = U[j] - m[j] * U[i]
-    return L, U
+def lu_decomposition(A):
+    #"""(I | A) -> (L | A)"""
+    m=3                    #Le asigno una valor cuaqluiera para que sea un float
+    U = copy.copy(A)
+    L = np.eye(len(A))
+    #print(f'Inicio:\nL:\n{L}\nU:\n{U}')  # comentar si no se quiere ver cada paso de la descomposicion
+    # k: numero de paso de la descomposicion
+    for k in range(len(U)):
+        # j: fila que se esta eliminando
+        for j in range(k+1, len(U)):
+            m = U[j][k]/U[k][k]
+            for w in range(len(U[0])):
+                U[j][w] = U[j][w] - m*U[k][w]
+            L[j][k] = m
+        #print(f'Paso {k + 1}:\nL:\n{L}\nU:\n{U}') # comentar si no se quiere ver cada paso de la descomposicion
+    return (L, U)
