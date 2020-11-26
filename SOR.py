8b348eb8ab76b7cf10cd3bb27014f906d7e6d617
AMartin99 <70243820+AMartin99@users.noreply.github.com>
2020-11-26 18:46:44 -0300

    comentarios sobre SOR

--- SOR.py
+++ SOR.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+#W debe estar entre 0 y 2 para que converga, si W=1 este este es el algoritmo de Gauss Seidel
 def sorCuadrada(A,b,tol,maxiter,w=1):
     n=np.size(b)
     x=np.ones(n)
@@ -8,3 +11,4 @@ def sorCuadrada(A,b,tol,maxiter,w=1):
                 if i!=j:
                     s=s+A[i,j]*x[j]
             x[i]=(1-w)*x[i]+(w/A[i,i])*(b[i]-s)
+    Return x
