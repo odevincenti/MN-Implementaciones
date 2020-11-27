12dd226f933071c2a2db1bf73773a4013ef2e16d
NicoPelayo <nifernandez@itba.edu.ar>
2020-11-26 21:17:14 -0300

    ej 3 parcial

--- DsvNicover.py
+++ DsvNicover.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import pandas as pd
+import matplotlib.pyplot as plt
 def leastsq(A,b):
     a=1
 #V,E,U = dvs(A,b)
@@ -19,11 +20,12 @@ def dvs(A,b):
         if ava[i]>0:
             U[:,i]=A @ V[:,i]/ E[i,i]
 
-
-
+#Nota ultra importante, el rango de A tiene que ser igual a la dimension del conjunto Fil(A), basicamente, que tenga mas
+#filas que columnas y sean un poco aleatorias
+#en caso contrario no funciona
 def leastsqrtrucho(A,b):
-    Aspade= np.linalg.inv(np.transpose(A) @ A) @ np.transpose(A)
-    x = Aspade @ b
+    Aspade= np.linalg.inv(np.transpose(A) @ A) @ np.transpose(A)    #calculo A espada
+    x = Aspade @ b                                                  #calculo x: A espada . b
     return x
 
 A = np.array([[1.02, 1], [1.01, 1], [0.94, 1], [0.99, 1]])
@@ -37,3 +39,23 @@ b = np.array([2.05, 1.99, 2.02, 1.93])
 # print(dvs(A))
 print("Lo que da:", leastsqrtrucho(A, b))
 print("Lo que debería dar:", np.linalg.lstsq(A, b, None)[0])
+df = pd.read_csv("p53.csv")
+x = np.array(df["x"].tolist())
+y = np.array(df["y"].tolist())
+n = x.shape[0]
+fig, ax = plt.subplots()
+ax.plot(x, y, ".", label="puntos")
+ax.legend()
+
+
+
+
+
+A=np.zeros((x.size,3))
+b=np.zeros(y.size)
+A[:,0]=np.sqrt(abs(x[:]))
+A[:,1]=np.cos(np.sqrt(abs(x[:])))
+A[:,2]=1
+print(leastsqrtrucho(A,y))
+print(np.linalg.lstsq(A,y))
+
